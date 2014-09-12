263762305db5868db2f5a9756f932f3893a59b70
Xavier Ordoquy <xordoquy@linovia.com>
2014-09-12 08:23:38 +0200

    Skips the __pycache__ directories (pyc/pyo for python2.7 / 3.x)

--- setup.py
+++ setup.py
@@ -20,9 +20,13 @@ def get_package_data(package):
     Return all files under the root package, that are not in a
     package themselves.
     """
+    def is_python_dir(dirpath):
+        return (os.path.exists(os.path.join(dirpath, '__init__.py')) or
+                '__pycache__' in dirpath)
+
     walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
             for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+            if not is_python_dir(dirpath)]
 
     filepaths = []
     for base, filenames in walk:
