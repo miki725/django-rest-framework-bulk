43f7b14296796566035b47f2689b6a3b020ab332
Miroslav Shubernetskiy <miroslav@miki725.com>
2015-02-07 23:30:55 -0500

    added BulkListSerializer and BulkSerializerMixin to add DRF3 support

---
+++ rest_framework_bulk/serializers.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, unicode_literals
+import rest_framework
+
+
+# import appropriate mixins depending on the DRF version
+# this allows to maintain clean code for each DRF version
+# without doing any magic
+# a little more code but a lit clearer what is going on
+if str(rest_framework.__version__).startswith('2'):
+    from .drf2.serializers import *  # noqa
+else:
+    from .drf3.serializers import *  # noqa
